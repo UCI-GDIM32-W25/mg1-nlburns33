65fbba8a41b12acbc390713c18ecf46d1cd60925
nlburns33 <nlburns@uci.edu>
2026-01-11 20:01:44 -0800

    Finished Features

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,32 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector2 movement = new Vector2(horizontal, vertical);
+        _playerTransform.Translate(movement * _speed * Time.deltaTime);
         
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft > 0)
+        {
+            _numSeedsLeft--;
+            _numSeedsPlanted++;
+            Instantiate(_plantPrefab, _playerTransform.position, _playerTransform.rotation);
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+        }
     }
 }
